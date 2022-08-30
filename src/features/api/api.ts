20d56cfcd19fa6439aaec9fa7fc20c9288787f15
Furkan Cat <furi544@gmail.com>
2022-08-30 15:48:00 +0300

    fixed api definations

--- src/features/api/api.ts
+++ src/features/api/api.ts
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export interface IPostResponse {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+  userId: number | undefined;
+  id: number | undefined;
+  title: string | undefined;
+  body: string | undefined;
 }
 
 export const apiSlice = createApi({
@@ -13,12 +13,12 @@ export const apiSlice = createApi({
     baseUrl: "https://jsonplaceholder.typicode.com/",
   }),
   endpoints: (builder) => ({
-    getPosts: builder.query<IPostResponse[], number | void>({
+    getPosts: builder.query<IPostResponse[], string | void>({
       query: () => ({
         url: "posts",
       }),
     }),
-    updatePost: builder.mutation<IPostResponse[], number>({
+    updatePost: builder.mutation<IPostResponse[], string | void>({
       query: (id) => ({
         url: `posts/${id}`,
       }),
