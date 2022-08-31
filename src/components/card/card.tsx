c7e9042f03580ec4929619c22b7996442177e210
Furkan Çat <furi544@gmail.com>
2022-08-31 09:54:36 +0300

    back buttton to card comp

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,16 +1,22 @@
 import { baseUrl } from "@constants/index";
 import { routes } from "@constants/routes";
-import { Link } from "react-router-dom";
+import { BiArrowBack } from "react-icons/bi";
 
 export interface ICardProps {
   title?: string;
   description?: string;
+  back?: any;
 }
 
 const Card = (props: ICardProps): JSX.Element => {
-  const { title, description } = props;
+  const { title, description, back } = props;
   return (
     <div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 min-h-full">
+      {back && (
+        <span className="rounded-lg bg-red shadow-md">
+          <BiArrowBack onClick={back} />
+        </span>
+      )}
       <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         {title}
       </h5>
