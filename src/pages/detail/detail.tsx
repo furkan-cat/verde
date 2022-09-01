b586a9c2134a40e67c1b6a63b4172cb2a51342de
Furkan Cat <furi544@gmail.com>
2022-09-01 09:06:37 +0300

    fixed typing logic on detail page

--- src/pages/detail/detail.tsx
+++ src/pages/detail/detail.tsx
@@ -1,10 +1,12 @@
+import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import Card from "@components/card/card";
-import Vstack from "@components/vstack/vstack";
 import TextInput from "@components/text-input/text-input";
-import Space from "@components/space/space";
+import Button from "@components/button/button";
+import { useAppDispatch } from "@features/hooks/hooks";
+import { useUpdatePostMutation } from "@features/api/api";
 
 interface ICardStateProps {
+  userId: number | undefined;
   id: number | undefined;
   title: string | undefined;
   body: string | undefined;
@@ -12,19 +14,39 @@ interface ICardStateProps {
 
 const Detail = () => {
   const data = useLocation().state as ICardStateProps;
-  const navigate = useNavigate();
+  const { id, userId, body, title } = data;
+  const [inputVal, setInputVal] = useState<string | undefined>("");
+  const [updatePost] = useUpdatePostMutation();
+  console.log(inputVal);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInputVal(e.target.value);
+  };
+
+  useEffect(() => {
+    setInputVal(data.body);
+  }, [data]);
+
+  const mutatedVal = {
+    id: id,
+    userId: userId,
+    body: inputVal,
+    title: title,
+  };
 
   return (
-    <Vstack center="items-center">
-      <TextInput text={data.title} />
-      <Space/>
-      <TextInput text={data.body} />
-      {/* <input type="text" value={data.body} /> */}
-      <div className="flex justify-between">
-        <button>Delete</button>
-        <button>Update</button>
+    <div className="flex flex-col content-center m-auto w-4/12 space-y-2 mt-4">
+      <div className="flex flex-col space-y-2">
+        <TextInput readOnlyText={data.title} readOnly={true} />
+        <TextInput onChange={handleChange} value={inputVal} />
       </div>
-    </Vstack>
+      <div className="flex justify-between">
+        <Button className="bg-red-600">Delete</Button>
+        <Button className="bg-sky-700" onClick={() => updatePost(mutatedVal)}>
+          Update
+        </Button>
+      </div>
+    </div>
   );
 };
 
