ea275717211748fa693c1c0ae9c8e351a6730afb
Furkan Çat <furi544@gmail.com>
2022-09-03 21:34:48 +0300

    added create new post button & delete mutation on detail page

--- src/pages/detail/detail.tsx
+++ src/pages/detail/detail.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import TextInput from "@components/text-input/text-input";
 import Button from "@components/button/button";
-import { useAppDispatch } from "@features/hooks/hooks";
-import { useUpdatePostMutation } from "@features/api/api";
+import {
+  useDeletePostMutation,
+  useUpdatePostMutation,
+} from "@features/api/api";
+import { FaBeer } from 'react-icons/fa';
 
 interface ICardStateProps {
   userId: number | undefined;
@@ -17,7 +20,8 @@ const Detail = () => {
   const { id, userId, body, title } = data;
   const [inputVal, setInputVal] = useState<string | undefined>("");
   const [updatePost] = useUpdatePostMutation();
-  console.log(inputVal);
+  const [deletePost] = useDeletePostMutation();
+  console.log(typeof id);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInputVal(e.target.value);
@@ -35,7 +39,12 @@ const Detail = () => {
   };
 
   return (
-    <div className="flex flex-col content-center m-auto w-4/12 space-y-2 mt-4">
+    <div className="flex flex-col content-center w-6/12 m-auto space-y-2 mt-4">
+      <div className="flex place-self-end">
+        <Button className="bg-sky-700" onClick={() => deletePost()} accesoryLeft={<FaBeer/>}>
+          Create New Post
+        </Button>
+      </div>
       <div className="flex flex-col space-y-2">
         <TextInput readOnlyText={data.title} readOnly={true} />
         <TextInput onChange={handleChange} value={inputVal} />
