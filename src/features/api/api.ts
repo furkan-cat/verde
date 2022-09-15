ab1f9c78c3c3e6b1af1a845c4e96c7fa35adf2cf
Furkan Cat <furi544@gmail.com>
2022-09-15 15:58:30 +0300

    apis transfered to JSON server API

--- src/features/api/api.ts
+++ src/features/api/api.ts
@@ -7,10 +7,15 @@ export interface IPostResponse {
   body: string | undefined;
 }
 
+/** 
+ * * Jsonplaceholder API => https://jsonplaceholder.typicode.com/
+ * * Json Server API => http://localhost:8000/
+**/
+
 export const apiSlice = createApi({
   reducerPath: 'postsApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://jsonplaceholder.typicode.com/',
+    baseUrl: 'http://localhost:8000/',
   }),
   endpoints: (builder) => ({
     getPosts: builder.query<IPostResponse[], string | void>({
@@ -33,7 +38,22 @@ export const apiSlice = createApi({
         };
       },
     }),
-    deletePost: builder.mutation<IPostResponse[], string | void>({
+    createPost: builder.mutation<string, IPostResponse>({
+      query: (data) => {
+        const { id, userId, title, body } = data;
+        return {
+          url: 'posts',
+          method: 'POST',
+          body: {
+            userId: userId,
+            id: id,
+            title: title,
+            body: body,
+          },
+        };
+      },
+    }),
+    deletePost: builder.mutation<IPostResponse[], number | void>({
       query: (id) => ({
         url: `posts/${id}`,
         method: 'DELETE',
@@ -42,4 +62,9 @@ export const apiSlice = createApi({
   }),
 });
 
-export const { useGetPostsQuery, useUpdatePostMutation, useDeletePostMutation } = apiSlice;
+export const {
+  useGetPostsQuery,
+  useUpdatePostMutation,
+  useCreatePostMutation,
+  useDeletePostMutation,
+} = apiSlice;
