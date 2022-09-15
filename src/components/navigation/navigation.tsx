7e8bec9719b05c0adb2e62b675db39bf83e8ad5f
Furkan Cat <furi544@gmail.com>
2022-09-15 15:57:08 +0300

    some fıxes ın navıgatıon

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import { FiPlay } from 'react-icons/fi';
 import { IoIosNotifications } from 'react-icons/io';
 import { MdWindow } from 'react-icons/md';
-import { Link } from 'react-router-dom';
-
+import { useAppSelector } from '@features/hooks/hooks';
 import { routes } from '@constants/routes';
 
 const Navigation = () => {
+  const state = useAppSelector((state) => state.ownPostsReducer.ownPosts);
   return (
     <nav className="bg-white px-2 flex justify-between p-2">
       <Link
@@ -15,8 +16,10 @@ const Navigation = () => {
         <FiPlay className=" hover:bg-slate-200" />
         <div className="text-lg font-medium">Arbit Blog</div>
       </Link>
-      <div className="flex justify-center items-center space-x-2">
-        <div>Posts</div>
+      <div className="flex justify-center items-center space-x-2 relative">
+        <p className="relative">
+          Posts <span className="absolute bottom-2 rıght-0">1</span>
+        </p>
         <IoIosNotifications className="text-gray-600 hover:text-gray-400" />
         <MdWindow />
         <div className="rounded-lg w-4 h-4 bg-slate-800" />
